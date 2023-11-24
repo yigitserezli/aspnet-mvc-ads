0b9b8d0d6b79950f6d1e3fcdcb72b8e1a2f95eaf
yigitserezli <y.serezli@outlook.com>
2023-11-24 23:03:42 +0200

    Views changes
    Views has been set according to instructions

---
+++ Ads.Web.Mvc/Controllers/AuthController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Ads.Web.Mvc.Controllers
+{
+    public class AuthController : Controller
+    {
+        // GET: /<controller>/
+        [HttpGet]
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult ForgotPassword()
+        {
+            return View();
+        }
+    }
+}
+
---
+++ Ads.Web.Mvc/Controllers/PageController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Ads.Web.Mvc.Controllers
+{
+    public class PageController : Controller
+    {
+        // GET: /<controller>/
+        [HttpGet]
+        public IActionResult Detail()
+        {
+            return View();
+        }
+    }
+}
+
